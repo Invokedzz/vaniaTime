ad6c856fcd67a69ff0540e99d83ab43de5d596fc
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-02 12:13:50 -0300

    adding some validators

--- src/controllers/validators.ts
+++ src/controllers/validators.ts
@@ -1,1 +1,63 @@
-// Interage com o model e view.
+import express, { Request, Response } from "express";
+
+import helmet from "helmet";
+
+import session from "express-session";
+
+// import { Sequelize } from "sequelize";
+
+// import connectSessionSequelize from 'connect-session-sequelize';
+
+import csurf from "csurf";
+
+import morgan from "morgan";
+
+import compression from "compression";
+
+const application = express();
+
+export function security (): void {
+
+    application.use(helmet({
+
+        contentSecurityPolicy: false,
+        dnsPrefetchControl: { allow: false },
+        frameguard: { action: 'deny' },
+        hidePoweredBy: true,
+        referrerPolicy: { policy: 'no-referrer' },
+        xssFilter: true,
+        hsts: { maxAge: 31536000, includeSubDomains: true, preload: true },
+        ieNoOpen: true,
+        noSniff: true,
+
+    }));
+
+    application.use(session({
+
+        secret: 'MAYBESOMETHINGDIFFERENTTHISTIME',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true, httpOnly: true },
+
+    }));
+
+    application.use(compression({
+
+        level: 9, 
+        threshold: 1024,
+        filter: (request: Request, response: Response) => {
+            return /text\/html/.test(response.getHeader('Content-Type') as string);
+        },
+        memLevel: 9, 
+        chunkSize: 16384, 
+
+    }));
+
+    const csrf = csurf({ cookie: { httpOnly: true, secure: true } });
+
+    application.use(csrf);
+
+    application.use(morgan('combined'));
+
+
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { startServer } from './website';
 
+import { security } from './controllers/validators';
+
+security();
+
 const gettingStarted = new startServer();
 
 gettingStarted.Listen();
