dcf5fa4229bee0a0bb5e475f1b8017499b1a3c19
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-02 09:20:34 -0300

    creating the structure

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { startServer } from './website';
+
+const gettingStarted = new startServer();
+
+gettingStarted.Listen();
---
+++ src/website.ts
@@ -0,0 +1,69 @@
+import express from 'express';
+
+import { rateLimit } from 'express-rate-limit';
+
+import { engine } from "express-handlebars";
+
+import path from "path";
+
+const application = express();
+
+const port = process.env.PORT || 3001;
+
+application.engine('handlebars', engine ({
+
+    defaultLayout: 'main',
+
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+
+}));
+
+application.set('view engine', 'handlebars');
+
+application.set('views', path.join(__dirname, 'views'));
+
+export class startServer {
+
+    private expressLimiter (): void {
+
+        const limiter = rateLimit({
+
+            windowMs: 15 * 60 * 1000, 
+
+            max: 100,
+
+            standardHeaders: true,
+
+            legacyHeaders: false,
+
+        });
+
+        application.use(limiter);
+
+    };
+
+    private changeSettings (): void {
+
+        application.use(express.json());
+
+        application.use(express.urlencoded({ extended: true }));
+
+        application.use(express.static('public'));
+
+    };
+
+    public Listen (): void {
+
+        this.expressLimiter();
+
+        this.changeSettings();
+
+        application.listen(port, (): void => {
+
+            console.log(`http://localhost:${port}`);
+
+        });
+
+    };
+
+};
