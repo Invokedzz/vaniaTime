f8438a3cd2b1f14aafdca88530657af7ebc96feb
Samuel Nóbrega <samuelnobrega902@gmail.com>
2024-10-03 16:09:58 -0300

    adding functions

--- src/controllers/validatorsInputs.ts
+++ src/controllers/validatorsInputs.ts
@@ -1,36 +1,36 @@
 import { body } from 'express-validator';
 
-export const validateLogin = [
-
-    body("email")
-    .isString()
-    .isEmail()
-    .isLength({ max: 255 })
-    .withMessage("Invalid e-mail address. Try again."),
-
-    body("password")
-    .isString()
-    .isLength({ min: 3, max: 30 })
-    .withMessage("Invalid password. Try again. Minimum 3 characters and maximum 30."),
-
-];
-
-export const validateRegister = [
+export function registerValidate (): void {
 
     body("username")
     .isString()
-    .isLength({ min: 1, max: 30 })
-    .withMessage("Invalid username. Try again. Minimum 1 character and maximum 30."),
+    .isLength({min: 1, max: 30})
+    .withMessage("Invalid username. Try again. Minimum: 1 character, maximum: 30 characters.");
     
     body("email")
-    .isString()
     .isEmail()
+    .isString()
     .isLength({ max: 255 })
-    .withMessage("Invalid e-mail address. Try again."),
-    
+    .withMessage("Invalid e-mail. Try again.");
+
     body("password")
     .isString()
-    .isLength({ min: 3, max: 30 })
-    .withMessage("Invalid password. Try again. Minimum 3 characters and maximum 30."),
+    .isLength({ min: 3, max: 255 })
+    .withMessage("Invalid password. Try again.");
 
-];
+};
+
+export function loginValidate (): void {
+
+    body("email")
+    .isEmail()
+    .isString()
+    .isLength({ max: 255 })
+    .withMessage("Invalid e-mail. Try again.");
+
+    body("password")
+    .isString()
+    .isLength({ min: 3, max: 255 })
+    .withMessage("Invalid password. Try again.");
+
+};
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { startServer } from './website';
 
 import { security } from './controllers/validators';
 
+//import { loginValidate, registerValidate } from './controllers/validatorsInputs';
+
 security();
 
 const gettingStarted = new startServer();
